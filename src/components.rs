6035f658c1976fcc4548c524445a7b27bad92012
Connor Mooneyhan <connmoo@gmail.com>
2022-02-05 15:37:11 -0500

    Refactor into components and systems modules

---
+++ src/components.rs
@@ -0,0 +1,12 @@
+use crate::prelude::*;
+
+#[derive(Component)]
+pub struct Paddle {
+     pub up_key: KeyCode,
+     pub down_key: KeyCode,
+}
+
+#[derive(Component)]
+pub struct Ball {
+    pub velocity: Vec3
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,33 @@
-use bevy::prelude::*;
-use bevy::sprite::collide_aabb::{ collide, Collision };
+#![warn(clippy::all, clippy::pedantic)]
 
-const SCREEN_WIDTH: f32 = 1200.0;
-const SCREEN_HEIGHT: f32 = 800.0;
-const PADDLE_WIDTH: f32 = 12.0;
-const PADDLE_HEIGHT: f32 = 100.0;
-const PADDLE_SPEED: f32 = 4.0;
-const BALL_RADIUS: f32 = 5.0;
-const BALL_SPEED: f32 = 5.0;
+mod systems;
+mod components;
 
-const TOP: f32 = SCREEN_HEIGHT / 2.0;
-const BOTTOM: f32 = -SCREEN_HEIGHT / 2.0;
-const RIGHT: f32 = SCREEN_WIDTH / 2.0;
-const LEFT: f32 = -SCREEN_WIDTH / 2.0;
-
-#[derive(Component)]
-struct Paddle {
-    up_key: KeyCode,
-    down_key: KeyCode,
+mod prelude {
+    pub use bevy::prelude::*;
+    pub use bevy::sprite::collide_aabb::{ collide, Collision };
+    pub const SCREEN_WIDTH: f32 = 1200.0;
+    pub const SCREEN_HEIGHT: f32 = 800.0;
+    pub const PADDLE_WIDTH: f32 = 12.0;
+    pub const PADDLE_HEIGHT: f32 = 100.0;
+    pub const PADDLE_SPEED: f32 = 4.0;
+    pub const BALL_RADIUS: f32 = 5.0;
+    pub const BALL_SPEED: f32 = 5.0;
+    pub const TOP: f32 = SCREEN_HEIGHT / 2.0;
+    pub const BOTTOM: f32 = -SCREEN_HEIGHT / 2.0;
+    pub const RIGHT: f32 = SCREEN_WIDTH / 2.0;
+    pub const LEFT: f32 = -SCREEN_WIDTH / 2.0;
+    pub use crate::components::*;
+    pub use crate::systems::*;
 }
 
-#[derive(Component)]
-struct Ball {
-    velocity: Vec3
-}
+use prelude::*;
 
 enum GameState {
     StartScreen,
     Playing,
     GameOver,
 }
-
 
 fn main() {
     App::new()
@@ -107,70 +104,3 @@ fn setup(mut commands: Commands) {
         });
 }
 
-fn game_state_systems() -> SystemSet {
-    SystemSet::new()
-        .with_system(paddle_movement_system)
-        .with_system(ball_collision_system)
-}
-
-fn paddle_movement_system(mut paddle_query: Query<(&mut Transform, &Paddle)>, keyboard_input: Res<Input<KeyCode>>) {
-    paddle_query.iter_mut().for_each(|(mut transform, paddle)| {
-        let y = &mut transform.translation.y;
-        if keyboard_input.pressed(paddle.up_key) {
-            *y += PADDLE_SPEED;
-        }
-        
-        if keyboard_input.pressed(paddle.down_key) {
-            *y += -PADDLE_SPEED;
-        }
-        *y = y.min(TOP - PADDLE_HEIGHT / 2.0).max(BOTTOM + PADDLE_HEIGHT / 2.0);
-    });
-
-}
-
-fn ball_collision_system(paddle_query: Query<&Transform, (With<Paddle>, Without<Ball>)>, mut ball_query: Query<(&mut Ball, &mut Transform)>) {
-    let (mut ball, mut ball_transform) = ball_query.single_mut();
-    let mut x_multiplier = 1.0;
-    let mut y_multiplier = 1.0;
-
-    for paddle_transform in paddle_query.iter() {
-        // Collide with paddle
-        let collision = collide(
-            ball_transform.translation,
-            ball_transform.scale.truncate(),
-            paddle_transform.translation,
-            paddle_transform.scale.truncate()
-        );
-
-        // Reflect off paddle
-        if let Some(collision) = collision {
-            match collision {
-                Collision::Left => x_multiplier = -1.0,
-                Collision::Top => y_multiplier = -1.0,
-                Collision::Right => x_multiplier = -1.0,
-                Collision::Bottom => y_multiplier = -1.0,
-            };
-        }
-    }
-    
-    // Reflect off edges of screen
-    if ball_transform.translation.y > TOP - BALL_RADIUS {
-        y_multiplier = -1.0;
-    }
-    if ball_transform.translation.y < BOTTOM + BALL_RADIUS {
-        y_multiplier = -1.0;
-    }
-    if ball_transform.translation.x > RIGHT - BALL_RADIUS {
-        x_multiplier = -1.0;
-    }
-    if ball_transform.translation.x < LEFT + BALL_RADIUS {
-        x_multiplier = -1.0;
-    }
-
-    // Reflect ball according to multipliers
-    ball.velocity.x *= x_multiplier;
-    ball.velocity.y *= y_multiplier;
-
-    // Update ball position
-    ball_transform.translation += ball.velocity;
-}
